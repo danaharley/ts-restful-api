d1550ecc2df17bfcbe03c0c4b8874b3853441a0b
danaharley <danaharley.github@gmail.com>
2024-04-10 21:26:41 +0700

    address api

--- test/test-util.ts
+++ test/test-util.ts
@@ -1,4 +1,4 @@
-import { Contact, User } from "@prisma/client";
+import { Address, Contact, User } from "@prisma/client";
 import { db } from "../src/application/db";
 import bcrypt from "bcryptjs";
 
@@ -70,5 +70,47 @@ export class ContactTest {
     }
 
     return contact;
+  }
+}
+
+export class AddressTest {
+  static async deleteAll() {
+    await db.address.deleteMany({
+      where: {
+        contact: {
+          username: "test",
+        },
+      },
+    });
+  }
+
+  static async create() {
+    const contact = await ContactTest.get();
+    await db.address.create({
+      data: {
+        contact_id: contact.id,
+        street: "Jalan test",
+        city: "Kota test",
+        province: "Provinsi test",
+        country: "Indonesia",
+        postal_code: "11111",
+      },
+    });
+  }
+
+  static async get(): Promise<Address> {
+    const address = await db.address.findFirst({
+      where: {
+        contact: {
+          username: "test",
+        },
+      },
+    });
+
+    if (!address) {
+      throw new Error("Address is not found");
+    }
+
+    return address;
   }
 }
