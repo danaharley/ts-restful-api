667599336b37e68402445b3bab8167e82c19c243
danaharley <danaharley.github@gmail.com>
2024-04-10 20:41:35 +0700

    search contact api

--- src/service/contact-service.ts
+++ src/service/contact-service.ts
@@ -3,12 +3,14 @@ import { db } from "../application/db";
 import {
   ContactResponse,
   CreateContactRequest,
+  SearchContactRequest,
   UpdateContactRequest,
   toContactResponse,
 } from "../model/contact-model";
 import { ContactValidation } from "../validation/contact-validation";
 import { Validation } from "../validation/validation";
 import { ResponseError } from "../error/response-error";
+import { Pageable } from "../model/page";
 
 export class ContactService {
   static async create(
@@ -84,5 +86,69 @@ export class ContactService {
     });
 
     return toContactResponse(contact);
+  }
+
+  static async search(
+    user: User,
+    request: SearchContactRequest
+  ): Promise<Pageable<ContactResponse>> {
+    const searchRequest = Validation.validate(
+      ContactValidation.SEARCH,
+      request
+    );
+
+    const skip = (searchRequest.page - 1) * searchRequest.size;
+
+    const filters = [];
+
+    if (searchRequest.name) {
+      filters.push({
+        OR: [
+          {
+            first_name: { contains: searchRequest.name },
+          },
+          {
+            last_name: { contains: searchRequest.name },
+          },
+        ],
+      });
+    }
+
+    if (request.email) {
+      filters.push({
+        email: { contains: searchRequest.email },
+      });
+    }
+
+    if (request.phone) {
+      filters.push({
+        phone: { contains: searchRequest.phone },
+      });
+    }
+
+    const contacts = await db.contact.findMany({
+      where: {
+        username: user.username,
+        AND: filters,
+      },
+      take: searchRequest.size,
+      skip: skip,
+    });
+
+    const total = await db.contact.count({
+      where: {
+        username: user.username,
+        AND: filters,
+      },
+    });
+
+    return {
+      data: contacts.map((contact) => toContactResponse(contact)),
+      paging: {
+        current_page: searchRequest.page,
+        total_page: Math.ceil(total / searchRequest.size),
+        size: searchRequest.size,
+      },
+    };
   }
 }
